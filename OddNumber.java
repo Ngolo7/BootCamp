18e07a0b1bd3709a3b92073bb93a3515488dedc7
Durga <55926544+Ngolo7@users.noreply.github.com>
2023-02-07 18:58:21 -0500

    Add files via upload

---
+++ OddNumber.java
@@ -0,0 +1,9 @@
+public class OddNumber {
+    public static void main(String[] args) {
+        int n=20;
+        for(int i=2; i<=n; i++){
+            if (i%2==1)
+                System.out.println("Odd Numbers are: "  +i);
+        }
+    }
+}
